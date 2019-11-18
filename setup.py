214e2409c48622e073e943339f4ffcd607953810
Nico Schlömer <nico.schloemer@gmail.com>
2019-11-18 15:29:18 +0100

    remove python2-compat code

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-import codecs
 import os
 
 from setuptools import find_packages, setup
@@ -10,10 +7,6 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 about = {}
 with open(os.path.join(base_dir, "pyfoobar", "__about__.py"), "rb") as f:
     exec(f.read(), about)
-
-
-def read(fname):
-    return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
 
 
 setup(
@@ -34,7 +27,7 @@ setup(
     author_email=about["__email__"],
     install_requires=[],
     description="A little bit of foobar in your life",
-    long_description=read("README.md"),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license=about["__license__"],
     classifiers=[
--- test/test_solve.py
+++ test/test_solve.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
-#
 import pyfoobar
 
 
 def test_solve():
     assert pyfoobar.solve() == 42
-    return
